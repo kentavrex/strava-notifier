27717161e130f1ab1ad3264841dc388f76953637
kentavrex <khanov.a@selectel.ru>
2024-02-23 16:02:47 +0300

    update readme

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Config(BaseSettings):
     TG_TOKEN: str
     KEYCLOACK_REFRESH_TOKEN: str
-    KEYCLOACK_CLIENT_ID: str
+    KEYCLOACK_CLIENT_ID: str = 'student-personal-cabinet'
     KEYCLOACK_TOKEN_URL: str = 'https://id.itmo.ru/auth'
     KEYCLOACK_REALM: str = 'itmo'
 
